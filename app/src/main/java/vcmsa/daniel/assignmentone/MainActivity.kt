807a1ae9f7f044b3de1941f3e0617b653aec4c2b
lab_services_student <qxsirdanielgamingxp@gmail.com>
2025-04-01 01:27:31 +0200

    Initial commit

--- app/src/main/java/vcmsa/daniel/assignmentone/MainActivity.kt
+++ app/src/main/java/vcmsa/daniel/assignmentone/MainActivity.kt
@@ -43,91 +43,39 @@ class MainActivity : AppCompatActivity() {
         // It simply serves as a hint for the user when they open the app.
         nextStep.setOnClickListener {
 
-        if (userTimeOfDay.text.toString().lowercase() == "morning") {
-            timesOfDay.text = " "
-            mealSuggestion.text =
-                "Breakfast Options: \nPorridge \nPancakes \nScrambled Eggs \nYogurt \nFruit"
+            if (userTimeOfDay.text.toString().lowercase() == "morning") {
+                timesOfDay.text = " "
+                mealSuggestion.text =
+                    "Breakfast Options: \nPorridge \nPancakes \nScrambled Eggs \nYogurt \nFruit"
 
-        }else if (userTimeOfDay.text.toString().lowercase() == "mid-morning") {
-            timesOfDay.text = " "
-            mealSuggestion.text =
-                "Brunch Options: \nFrench Toast \nCheese and Ham Sandwich \nAvocado on Toast " +
-                        "\nFruit Salad \nCake and Tea"
+            }else if (userTimeOfDay.text.toString().lowercase() == "mid-morning") {
+                timesOfDay.text = " "
+                mealSuggestion.text =
+                    "Brunch Options: \nFrench Toast \nCheese and Ham Sandwich \nAvocado on Toast " +
+                            "\nFruit Salad \nCake and Tea"
 
-        } else if (userTimeOfDay.text.toString().lowercase() == "afternoon") {
-                        timesOfDay.text = " "
-                        mealSuggestion.text =
-                            "Lunch Options: \nGrilled Cheese Sandwich \nBurger and Chips \nHot-dogs " +
-                                    "\nChicken and Avocado Salad \nBeef Wrap."
+            } else if (userTimeOfDay.text.toString().lowercase() == "afternoon") {
+                            timesOfDay.text = " "
+                            mealSuggestion.text =
+                                "Lunch Options: \nGrilled Cheese Sandwich \nBurger and Chips \nHot-dogs " +
+                                        "\nChicken and Avocado Salad \nBeef Wrap."
 
-        }else if (userTimeOfDay.text.toString().lowercase() == "mid-afternoon") {
-                        timesOfDay.text = " "
-                        mealSuggestion.text = "Afternoon-snack Options: \nMicrowave-Muffins " +
-                                "\nPeanut Butter Bars \nApple chips \nPop-corn \n"
+            }else if (userTimeOfDay.text.toString().lowercase() == "mid-afternoon") {
+                            timesOfDay.text = " "
+                            mealSuggestion.text = "Afternoon-snack Options: \nMicrowave-Muffins " +
+                                    "\nPeanut Butter Bars \nApple chips \nPop-corn \n"
 
-        } else if (userTimeOfDay.text.toString().lowercase() == "dinner") {
-            timesOfDay.text = " "
-            mealSuggestion.text = "Dinner Options:\nLasagna \nButter Chicken Curry \nPasta and Meatballs  " +
-                    "\nPizza  \nSteak with Potatoes and Gravy"
+            } else if (userTimeOfDay.text.toString().lowercase() == "dinner") {
+                timesOfDay.text = " "
+                mealSuggestion.text = "Dinner Options:\nLasagna \nButter Chicken Curry \nPasta and Meatballs  " +
+                        "\nPizza  \nSteak with Potatoes and Gravy"
 
-        } else {
-            timesOfDay.text = " "
-            mealSuggestion.text = "Invalid input. Numbers, special characters and misspelled words are not " +
-                    "allowed.\nPlease enter a valid time of day." +
-                    "\nFor example: \nMorning \nMid-morning \nAfternoon \nMid-afternoon \nDinner"
+            } else {
+                timesOfDay.text = " "
+                mealSuggestion.text = "Invalid input. Numbers, special characters and misspelled words are not " +
+                        "allowed.\nPlease enter a valid time of day." +
+                        "\nFor example: \nMorning \nMid-morning \nAfternoon \nMid-afternoon \nDinner"
 }
 }
     }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
